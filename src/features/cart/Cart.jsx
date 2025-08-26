d31669b10115da7b6698d57bfbb761e51410c628
Adelina <yadelishka@gmail.com>
2025-08-26 22:10:59 +0400

    integrate geolocation

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,12 +1,12 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useDispatch, useSelector } from 'react-redux';
-import { clearCart, getCart, getUsername } from './cartSlice';
 import EmptyCart from './EmptyCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
 
 function Cart() {
-  const username = useSelector(getUsername);
+  const username = useSelector((state) => state.user.username);
   const cart = useSelector(getCart);
   const dispatch = useDispatch();
 
